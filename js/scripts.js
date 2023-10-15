b489f189fabb156cf8ed0c047a72988ce0896612
graciari2000 <92252649+graciari2000@users.noreply.github.com>
2023-10-15 15:38:25 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
 // countdown.js
+// countdown.js
+
+const websiteRelatedOneLiners = [
+    "We're sprucing up. Stay tuned for the makeover!",
+    "We're under construction. Sorry for the dust!",
+    "Hang tight! We're getting a facelift.",
+    "We're taking a short nap. We'll wake up soon!",
+    "Why did we go to therapy? We had too many broken links!",
+    "We're like fine wine—we're aging gracefully!",
+    "Don't worry, we'll be back with a fresh look!",
+];
+
+function getRandomWebsiteOneLiner() {
+    const randomIndex = Math.floor(Math.random() * websiteRelatedOneLiners.length);
+    return websiteRelatedOneLiners[randomIndex];
+}
+
+// Rest of the code remains the same
+
 function updateCountdown() {
     const endDate = new Date("2024-03-30T00:00:00").getTime();
     const now = new Date().getTime();
