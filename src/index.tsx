50702ae6dea06ca674aff4d1a54b89aba55381a4
graciari2000 <ariane-gracia.akakpo@epitech.eu>
2025-09-23 07:18:01 +0100

    Update index.html to point to src/index.tsx instead of main.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './src/App';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
